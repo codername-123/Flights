231ea30093dad17d46ff5651fdda38773d49a719
codername-132 <nitin.rawat629@gmail.com>
2023-05-30 13:30:46 +0530

    Created middleware to validate request body for createAirplane controller

---
+++ src/middlewares/airplane-middleware.js
@@ -0,0 +1,17 @@
+const { ErrorResponse } = require("../utils/common");
+const { StatusCodes } = require("http-status-codes");
+const AppError = require("../utils/errors/app-error");
+
+function validateCreateRequestBody(req, res, next) {
+  if (!req.body.modelNumber || !req.body.capacity) {
+    ErrorResponse.message = "Something went wrong";
+    ErrorResponse.error = new AppError(
+      ["you have not sent the correct request body"],
+      StatusCodes.BAD_REQUEST
+    );
+    return res.status(ErrorResponse.error.statuscode).json(ErrorResponse);
+  }
+  next();
+}
+
+module.exports = { validateCreateRequestBody };
