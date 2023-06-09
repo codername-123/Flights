8aa66b4646cd4b6348ddfcd2f28bba40f87b9ead
codername-132 <nitin.rawat629@gmail.com>
2023-06-09 11:35:28 +0530

    Added enums for defining a seat type

---
+++ src/utils/common/enums.js
@@ -0,0 +1,10 @@
+const SEAT_TYPE = {
+  BUSINESS: "business",
+  FIRST_CLASS: "first-class",
+  ECONOMY: "economy",
+  PREMIUM_ECONOMY: "premium-economy",
+};
+
+module.exports = {
+  SEAT_TYPE,
+};
