82f63b57c8409c0c3ee15704e6f26f764d8ee450
codername-132 <nitin.rawat629@gmail.com>
2023-06-26 16:24:09 +0530

    update the string so that it's more clear when checking which api is live among many others

--- src/controllers/Info-controller.js
+++ src/controllers/Info-controller.js
@@ -2,7 +2,12 @@ const { StatusCodes } = require("http-status-codes");
 function Info(req, res) {
   res
     .status(StatusCodes.OK)
-    .json({ success: true, message: "API is live", error: {}, data: {} });
+    .json({
+      success: true,
+      message: "Flight Service API is live",
+      error: {},
+      data: {},
+    });
 }
 
 module.exports = { Info };
